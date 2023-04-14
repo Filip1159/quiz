df76cf551d5efbfec834c14632650e888f611f6f
Filip Wiśniewski <fwfw541111@gmail.com>
2023-04-14 10:33:20 +0200

    Add useParams

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import {SlideComponent} from "./components/SlideComponent";
 import {Route, BrowserRouter, Switch} from "react-router-dom";
 
 function App() {
-
     return (
         <div className="App">
             <BrowserRouter>
                 <Switch>
-                    <Route exact path="/slides/programmer">
-                        <SlideComponent/>
+                    <Route path="/slides/:topic/:slideNumber">
+                        <SlideComponent />
                     </Route>
                     <Route path=""/>
                 </Switch>
--- src/components/SlideComponent.js
+++ src/components/SlideComponent.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './../styles/SlideComponent.css'
+import {useParams} from "react-router-dom";
 
 export const SlideComponent = () => {
+    const { topic, slideNumber } = useParams();
+
     return (
         <div>
-            <h1>Cechy dobrego programisty</h1>
+            <h1>{`Cechy dobrego programisty ${topic}, ${slideNumber}`}</h1>
             <img className="slideImg" src="/img/programmer_images/slide1.jpg" alt="slide" />
             <div className="slideNavigation">
                 <button className="button">&lt; Previous</button>
